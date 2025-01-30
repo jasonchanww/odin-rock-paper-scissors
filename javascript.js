0f65cbe3dad070d69bc98f96af16e36f192eb5bb
Jason Chan <jasonchanww@gmail.com>
2025-01-30 16:25:42 +0800

    Add function of score calculation of single round

--- javascript.js
+++ javascript.js
@@ -16,6 +16,33 @@ function getComputerChoice(){
 
 
 function getHumanChoice(){
-    let humanChoice = prompt("get human choice");
+    let humanChoice = ""+prompt("get human choice");
+    return humanChoice.toLowerCase();
 }
 
+
+let computerScore = 0;
+let humanScore = 0;
+
+
+function playRound(humanChoice, computerChoice) {
+    if(humanChoice == computerChoice) return;
+    if(humanChoice == "rock"){
+        computerChoice == "paper" ? computerScore++ : humanScore++;
+    }
+    else if(humanChoice == "paper"){
+        computerChoice == "rock" ? humanScore++ : computerScore++;
+    }
+    else if(humanChoice == "scissors"){
+        computerChoice == "rock" ? computerScore++ : humanScore++;
+    }
+    else{
+        computerScore++;
+    }
+}
+  
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+  
