7c3d3ad7891a3d398a8aaf7f31b58832a5767b46
Jason Chan <jasonchanww@gmail.com>
2025-01-30 16:14:07 +0800

    Add function of getting random choice of rock paper scissors

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
-console.log("Hello World")
+function getComputerChoice(){
+    let randomNum = Math.floor(Math.random() * 3);
+    switch(randomNum){
+        case 0:
+            return "rock";
+            break;
+        case 1:
+            return "paper";
+            break;
+        case 2:
+            return "scissors";
+            break;
+    }
+    return "unknown";
+}
